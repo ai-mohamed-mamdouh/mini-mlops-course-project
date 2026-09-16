a9c3a51a7c1f930da65023c49e7d4e42a56121d7
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-09-16 15:33:07 +0300

    feat: test_preprocessing

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,22 @@
+from mini_mlopscourse_project.schemas.iris.input_schema import InputSchema
+from mini_mlopscourse_project.processors.iris.iris_processor import IrisProcessor
+
+
+def test_preprocessing():
+
+    # Arrange
+    input_data = InputSchema(
+        features=[ [1, 2, 3, 4] ]
+    )
+
+    # Act
+    result = IrisProcessor().pre_processing(input_data)
+
+    # Assert
+    assert isinstance(result, InputSchema)
+
+    assert isinstance(result.features, list)
+    assert isinstance(result.features[0], list)
+
+    assert len(result.features) == 1
+    assert len(result.features[0]) == 4
