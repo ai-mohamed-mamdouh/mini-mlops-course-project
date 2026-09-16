8dffb94291fb17301ddb2f048a6c4fd18a61c630
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-09-16 15:39:13 +0300

    feat: add test_prostprocessing

---
+++ tests/test_postprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from mini_mlopscourse_project.processors.iris.iris_processor import IrisProcessor
+from mini_mlopscourse_project.schemas.iris.output_schema import OutputSchema
+
+
+def test_postprocessing():
+
+    # Arrange
+    logits = np.array([
+        [1.0, 2.0, 5.0]
+    ])
+
+    # Act
+    result = IrisProcessor().post_processing(logits)
+
+    # Assert
+    assert isinstance(result, OutputSchema)
+
+    assert isinstance(result.preds, list)
+    assert isinstance(result.proba, list)
+
+    assert result.preds == [2]
+
+    assert len(result.proba) == 1
+    assert len(result.proba[0]) == 3
