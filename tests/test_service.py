341b8a8aee26d150a2046f8d0333f94bb2396cce
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-09-16 15:52:23 +0300

    feat: add test_service

---
+++ tests/test_service.py
@@ -0,0 +1,25 @@
+from mini_mlopscourse_project.services.iris.iris_service import IrisService
+from mini_mlopscourse_project.models.factory import ModelFactory
+from mini_mlopscourse_project.processors.iris.iris_processor import IrisProcessor
+from mini_mlopscourse_project.schemas.iris.input_schema import InputSchema
+from mini_mlopscourse_project.schemas.iris.output_schema import OutputSchema
+
+def test_service() :
+    input_data = InputSchema(features=[ [1,2,3,4] ])
+
+    result = IrisService(
+        ModelFactory.create('iris'),
+        iris_processor=IrisProcessor()
+        ).run_iris_service(input_data=input_data)
+
+    # Assert
+    assert isinstance(result, OutputSchema)
+
+    assert isinstance(result.preds, list)
+    assert isinstance(result.proba, list)
+
+    assert result.preds == [2]
+
+    assert len(result.proba) == 1
+    assert len(result.proba[0]) == 3
+    
