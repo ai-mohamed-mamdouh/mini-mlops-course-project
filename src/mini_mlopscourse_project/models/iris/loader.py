7424c0022f0542241e0481b36d90420486cfa456
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-09-18 12:14:28 +0300

    fix: handle model loading errors

--- src/mini_mlopscourse_project/models/iris/loader.py
+++ src/mini_mlopscourse_project/models/iris/loader.py
@@ -1,9 +1,16 @@
 import onnxruntime as ort
 from pathlib import Path
+from mini_mlopscourse_project.core.exceptions import ( ModelLoadError )
 
 
 def loader(path: Path):
     """Load an ONNX model from a given path and return its inference session."""
-    
-    session = ort.InferenceSession(path)
-    return session
+
+    try:
+        session = ort.InferenceSession(path)
+        return session
+
+    except Exception as e:
+        raise ModelLoadError(
+            "Cannot load model"
+        ) from e
